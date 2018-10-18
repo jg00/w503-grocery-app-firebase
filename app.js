1e9498b520b33906aa611715be845338e4fc6d27
Jack Gamboa <jgamboa929@gmail.com>
2018-10-18 17:37:18 -0500

    Version 2 - added category, items, display

--- app.js
+++ app.js
@@ -1,32 +1,127 @@
 
+const database = firebase.database();
+const categoriesRef = database.ref('categories');
 
-var database = firebase.database();
 
 // Add categories
-$("#btnAddGroceryCategory").click(function() {
-    addCategory() 
+$("#btnAddCategory").click(function() {
+    let txtCategory = $("#txtCategory").val();
+    let category = { title: txtCategory, zipCode: "77777"}
+
+    if (txtCategory) {
+        addCategory(category) 
+        $("#txtExists").show();
+    }
+    else {
+        console.log("test")
+        $("#txtExists").hide();
+    }
 });
 
-function addCategory() {
-    let groceryCategoryVal = $("#txtGroceryCategory").val();
-    
-    database.ref('GroceryCategory/' + groceryCategoryVal).set({name: groceryCategoryVal});
+
+function addCategory(category) {
+
+    let refCheck = categoriesRef.child(category.title);
+
+    refCheck.once("value") 
+      .then(function(snapshot) {
+
+        if (snapshot.exists()) {
+            $("#txtExists").text("Category Exists")
+        }
+        else {
+            categoriesRef.child(category.title).set(category)
+            $("#txtExists").empty();
+        }    
+      });
 }
 
 
+// Configure the value event generated from firebase database
+function configureObservers() {
 
-// Add items
-$("#btnAddGroceryItem").click(function() {
-    addItemsToCategory();
+    categoriesRef.on('value',function(snapshot){
+  
+        categories = []
+  
+        snapshot.forEach(function(childSnapshot){
+            categories.push(childSnapshot.val())
+    })
+  
+        displayCategories(categories)
+    })
+  }
+
+configureObservers()
+
+
+function displayCategories(categories) {
+
+    let liItem2 = ""
+    let liItem3 = ""
+    let liCombined = ""
+
+    for (let key of categories) {
+
+        let category = key.title;
+
+        liItem2 = ""
+        liItem2 = `<li>
+                        <span class="classCategory">${category}</span>
+                  </li>`
+
+        liItem3 = ""
+        for (let item in key.items) {
+            
+            let listItem = item;
+
+            liItem3 += `<ul id="ulItemParent">
+                            <li>
+                                <span class="classItem">${listItem}</span>
+                                <button type="button" onclick="removeItem(this)">Remove</button>
+                            </li>
+                        </ul>`
+        }
+
+        liCombined += liItem2 + liItem3
+    }
+
+    ulList.innerHTML = liCombined;
+}
+
+
+$("#btnAddItem").click(function() {
+    let txtCategory = $("#txtCategory").val();
+    let txtItem = $("#txtItem").val();
+    
+    let item = { title: txtItem, quantity: 5 }
+    addGroceryItem(txtCategory, item) 
 });
 
-function addItemsToCategory() {
-    let groceryCategoryVal = $("#txtGroceryCategory").val();
-    let groceryItemVal = $("#txtGroceryItem").val();
-    
-    database.ref("GroceryCategory/" + groceryCategoryVal + "/Items")
 
-        .set({item: groceryItemVal});
-  
-    
+// Add item to category
+function addGroceryItem(txtCategory, item) {
+
+    let itemsRef = categoriesRef.child(txtCategory).child("items")
+    itemsRef.child(item.title).set(item)
+
+    resetTextBoxes()
+}
+
+
+// Clear textboxes for next item to add
+function resetTextBoxes(){
+    $("#txtItem").val('');
+    $("#txtExists").val('');
+}
+
+
+function removeItem(sender) {
+
+    // Section to try to remove item from database but in progress
+    let classCategory = $(sender).parent().parent().parent().find('.classCategory').text(); 
+    let itemCategory = $(sender).parent().find('.classItem').text(); // works
+
+    // REemove from front end
+    $(sender).parent().remove();
 }
